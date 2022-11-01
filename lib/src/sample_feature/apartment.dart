59609c21ce560edc7302513d55d35fa56154606e
cluedia <claudiahahn00@gmail.com>
2022-11-01 16:39:41 +0100

    change from sample to appartment entity

---
+++ lib/src/sample_feature/apartment.dart
@@ -0,0 +1,5 @@
+class Appartment {
+  const Appartment(this.id);
+
+  final int id;
+}
--- lib/src/sample_feature/apartment_item_list_view.dart
+++ lib/src/sample_feature/apartment_item_list_view.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
 
 import '../settings/settings_view.dart';
-import 'sample_item.dart';
-import 'sample_item_details_view.dart';
+import 'apartment.dart';
+import 'apartment_details_view.dart';
 
 /// Displays a list of SampleItems.
 class SampleItemListView extends StatelessWidget {
   const SampleItemListView({
     super.key,
-    this.items = const [SampleItem(1), SampleItem(2), SampleItem(3)],
+    this.items = const [Appartment(1), Appartment(2), Appartment(3)],
   });
 
   static const routeName = '/';
 
-  final List<SampleItem> items;
+  final List<Appartment> items;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sample Items'),
+        title: const Text('Apartments'),
         actions: [
           IconButton(
             icon: const Icon(Icons.settings),
@@ -49,21 +49,20 @@ class SampleItemListView extends StatelessWidget {
           final item = items[index];
 
           return ListTile(
-            title: Text('SampleItem ${item.id}'),
-            leading: const CircleAvatar(
-              // Display the Flutter Logo image asset.
-              foregroundImage: AssetImage('assets/images/flutter_logo.png'),
-            ),
-            onTap: () {
-              // Navigate to the details page. If the user leaves and returns to
-              // the app after it has been killed while running in the
-              // background, the navigation stack is restored.
-              Navigator.restorablePushNamed(
-                context,
-                SampleItemDetailsView.routeName,
-              );
-            }
-          );
+              title: Text('Apartment ${item.id}'),
+              leading: const CircleAvatar(
+                // Display the Flutter Logo image asset.
+                foregroundImage: AssetImage('assets/images/flutter_logo.png'),
+              ),
+              onTap: () {
+                // Navigate to the details page. If the user leaves and returns to
+                // the app after it has been killed while running in the
+                // background, the navigation stack is restored.
+                Navigator.restorablePushNamed(
+                  context,
+                  SampleItemDetailsView.routeName,
+                );
+              });
         },
       ),
     );
